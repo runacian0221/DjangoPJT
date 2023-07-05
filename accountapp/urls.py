5590940626c7e3d49fd46e4e7175816bc0c5904f
runacian0221 <pil0610@naver.com>
2023-07-05 16:22:31 +0900

    Initial commit

---
+++ accountapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from accountapp.views import hello_world
+
+app_name = 'accountapp'
+
+# name을 사용하면 'accountapp:hello_world' 이런 방식으로도 호출이 가능하다.
+
+urlpatterns = [
+    path('hello_world/', hello_world, name='hello_world')
+]
