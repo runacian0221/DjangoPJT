d160263681bd14cac97775d01fafdbf1729b72ef
runacian0221 <pil0610@naver.com>
2023-07-10 16:27:54 +0900

    django course 18 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from accountapp.models import HelloWorld
+
 # Create your views here.
 
 
@@ -7,6 +10,15 @@ def hello_world(request):
     # return HttpResponse('안녕하세요')
 
     if request.method == "POST":
-        return render(request, 'accountapp/hello_world.html', context={'text':'POST METHOD!!'})
+
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        # 모든 오브젝트를 가져오기
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text':'GET METHOD!!'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
