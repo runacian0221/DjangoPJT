2fae0d9fa83b595b1661e216395748c7acbc1d4e
runacian0221 <pil0610@naver.com>
2023-09-20 22:45:58 +0900

    add deleteview

--- projectapp/views.py
+++ projectapp/views.py
@@ -2,11 +2,11 @@ from typing import Any, Dict
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic.list import MultipleObjectMixin
 
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, DeleteView
 from articleapp.models import Article
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
@@ -51,3 +51,10 @@ class ProjectListView(ListView):
     context_object_name = 'project_list'
     template_name = 'projectapp/list.html'
     paginate_by = 25
+
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
+class ProjectDeleteView(DeleteView):
+    model = Project
+    template_name = 'projectapp/delete.html'
+    success_url = reverse_lazy('projectapp:list')
